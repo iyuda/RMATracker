725601abc837a7c207bbb728c16e620c21371c84
Iy0611 <iyuda@hotmail.com>
2018-06-18 22:56:59 -0400

    Initial commit

---
+++ RMATracker/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RMATracker.Models;
+
+namespace RMATracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            //Retrieve new rma from DB
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendRMAData()
+        {
+            RMATracker.Models.RMATracker rma = new Models.RMATracker();
+            rma.TrackerID = Request["txtTrackId"];
+            rma.RMAType = Request["TypeofRMA"];
+
+            //Send to db...
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ RMATracker/Models/RMATracker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RMATracker.Models
+{
+    public class RMATracker
+    {
+        public string TrackerID { get; set; }
+        public string RMAType { get; set; }
+    }
+}
